a4d1ae95913b2133fde2a8138ed5f3a9f97a1ef9
Colin Lee <clee111@student.cscc.edu>
2026-09-15 13:04:29 -0400

    Update 7-8_deli.py

--- 7-8_deli.py
+++ 7-8_deli.py
@@ -1,1 +1,16 @@
+sandwich_orders = ["tuna", "turkey", "ham", "chicken"]
+finished_sandwiches = []
+
+while sandwich_orders:
+  current_sandwich = sandwich_orders.pop(0)
+  
+  print(f"I made your {current_sandwich} sandwich.")
+  
+  finished_sandwiches.append(current_sandwich)
+
+print("\nThe following sandwiches were made:")
+
+for sandwich in finished_sandwiches:
+  print(sandwich)
+
 
